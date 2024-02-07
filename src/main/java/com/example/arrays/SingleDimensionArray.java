e0273f0fbab1a35671599d7ae672130e6b4504f0
Saindla Manoj Kumar <SA20182158@wipro.com>
2024-02-07 14:38:05 +0530

    Arrays example

---
+++ src/main/java/com/example/arrays/SingleDimensionArray.java
@@ -0,0 +1,20 @@
+package com.example.arrays;
+
+public class SingleDimensionArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int[] j = new int[5];
+		
+		j[0]= 5;
+		j[2]=8;
+		j[4]=9;
+for(int i=0;i<j.length;i++) {
+		System.out.println(j[i]);
+	}
+	}
+
+}
